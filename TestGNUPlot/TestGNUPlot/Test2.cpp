c2c643818a74d6672a8006b443bde482d9bddfe9
itachi1706 <kennethsohyq@hotmail.com>
2015-01-14 11:57:41 +0800

    Added Sample for GNUPlot

---
+++ TestGNUPlot/TestGNUPlot/Test2.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define NUM_POINTS 5
+#define NUM_COMMANDS 2
+
+int main(){
+	double xvals[NUM_POINTS] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+	double yvals[NUM_POINTS] = { 5.0, 3.0, 1.0, 3.0, 5.0 };
+	/*Opens an interface that one can use to send commands as if they were typing into the
+	*     gnuplot command line.  "The -persistent" keeps the plot open even after your
+	*     C program terminates.
+	*/
+	FILE * gnuplotPipe = _popen("gnuplot -persistent", "w");
+	fprintf(gnuplotPipe, "set title \"Lol\" \n");
+	fprintf(gnuplotPipe, "plot '-' w linesp \n");
+	int i;
+	for (i = 0; i < NUM_POINTS; i++)
+	{
+		fprintf(gnuplotPipe, "%lf %lf \n", xvals[i], yvals[i]); //Write the data to a temporary file
+	}
+	fprintf(gnuplotPipe, "e");
+	
+
+	/*
+	for (i = 0; i < NUM_COMMANDS; i++)
+	{
+		fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
+	}*/
+	fflush(gnuplotPipe);
+	
+	return 0;
+}
